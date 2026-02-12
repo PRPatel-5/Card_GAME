831b158400d83f7c1efe79a76504063f501f5578
PRPatel-5 <princeranpariya002@gmail.com>
2026-02-12 12:01:45 +0530

    first commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,21 +1,10 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:memory_garden/main.dart';
+import 'package:vita_mahjong/main.dart';
 
 void main() {
-  testWidgets('Memory Garden app loads', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MemoryGardenApp());
-    
-    // Just verify the app loads without crashing
+  testWidgets('App smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const VitaMahjongApp());
     expect(find.byType(MaterialApp), findsOneWidget);
   });
 }
